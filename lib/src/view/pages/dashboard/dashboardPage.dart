7661d13bf2258029f163e819c13bb8b250de5173
fsdramjan <fsd.ramjan@gmail.com>
2025-03-24 13:29:02 +0600

    loading animation added

--- lib/src/view/pages/dashboard/dashboardPage.dart
+++ lib/src/view/pages/dashboard/dashboardPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fsdramjan/src/service/responsive/responsiveLayout.dart';
 import 'package:fsdramjan/src/service/webHeaderText.dart';
+import 'package:fsdramjan/src/view/pages/dashboard/responsive/desktopDashboard.dart';
 import 'package:fsdramjan/src/view/pages/dashboard/responsive/mobileDashboard.dart';
+import 'package:fsdramjan/src/view/pages/dashboard/responsive/tabletDashboard.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
@@ -10,8 +12,8 @@ class DashboardPage extends StatelessWidget {
       headerText: 'Fsd Ramjan',
       child: ResponsiveLayout(
         mobileBody: MobileDashboard(),
-        desktopBody: MobileDashboard(),
-        tabletBody: MobileDashboard(),
+        desktopBody: DesktopDashboard(),
+        tabletBody: TabletDashboard(),
         // desktopBody: DesktopDashboard(),
         // tabletBody: TabletDashboard(),
       ),
